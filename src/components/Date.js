6d015861ce790356dcd278fb9c7696b3f613b478
Frantass <88505051+MFranco1010@users.noreply.github.com>
2023-09-19 21:59:37 -0300

    funcional

--- src/components/Date.js
+++ src/components/Date.js
@@ -1,38 +1,55 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import moment from 'moment';
-moment().format();
-var now = moment()
+import 'moment/locale/es';
 
 export const Date = () => {
-    const [jump, setJump] = useState(0);
-    const plusJump = () => {
-        setJump(jump + 1);
-      };
-    const minusJump = () => {
-        setJump(jump - 1);
-    };
-    const [date, setDate] = useState(moment());
-    const jumpDate = () => {
-        const noww = now.clone().add(jump, 'day');
-        setDate(noww);
-      };
-    const mjumpDate = () => {
-    const noww = now.clone().subtract(jump, 'day');
-    setDate(noww);
-    };
+  const [jump, setJump] = useState(0);
+  const [date, setDate] = useState(moment());
+  const [hour, setHour] = useState(moment());
+
+  const plusJump = () => {
+    setJump(jump + 1);
+  };
+  const minusJump = () => {
+    setJump(jump - 1);
+  };
+
+  const jumpDate = () => {
+    const newDate = date.clone().add(jump, 'day');
+    setDate(newDate);
+  };
+  const mjumpDate = () => {
+    const newDate = date.clone().subtract(jump, 'day');
+    setDate(newDate);
+  };
+
+  const jumpHour = () => {
+    const newHour = hour.clone().add(1, 'hour');
+    setHour(newHour);
+  };
+  const mjumpHour = () => {
+    const newHour = hour.clone().subtract(1, 'hour');
+    setHour(newHour);
+  };
 
   return (
     <div className='box'>
-        <div className='btns'>
-            <button className='btn' onClick={minusJump}>-</button>
-            <p>Valor: {jump}</p>
-            <button className='btn' onClick={plusJump}>+</button>
-        </div>
-        <div className='btns'>
-            <button className='btn' onClick={mjumpDate}>-</button>
-            <p>Valor: {date}</p>
-            <button className='btn' onClick={jumpDate}>+</button>
-        </div>
+      <div className='btns'>
+        <button className='btn' onClick={minusJump}>-</button>
+        <p>Días: {jump}</p>
+        <button className='btn' onClick={plusJump}>+</button>
+      </div>
+      <div className='btns'>
+        <button className='btn' onClick={mjumpHour}>-</button>
+        <p>Horas</p>
+        <button className='btn' onClick={jumpHour}>+</button>
+      </div>
+      <div className='btns'>
+        <button className='btn' onClick={mjumpDate}>-</button>
+        <p>Update fecha</p>
+        <button className='btn' onClick={jumpDate}>+</button>
+      </div>
+      <p className='now'>{date.format('LLLL')}</p>
     </div>
-  )
-}
+  );
+};
